b49139d21cf903abe70214fbca3bac2911be0892
5hailesh-p <shailesh.p2701@gmail.com>
2025-08-21 08:24:45 +0530

    footer added and content also

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,56 @@
 function Footer() {
-    return(
+    return (
         <>
-        <h5 className="">Footer</h5>
+            <footer className="bg-dark text-light pt-5 pb-4" bg="bg3">
+                <div className="container text-center text-md-start">
+                    <div className="row text-center text-md-start">
+
+
+                        <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                            <h5 className="text-uppercase mb-4 fw-bold text-info">Your Name</h5>
+                            <p>
+                                Creative web developer specializing in front-end development. Passionate about clean code, responsive design, and beautiful UI.
+                            </p>
+                        </div>
+
+
+                        <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                            <h5 className="text-uppercase mb-4 fw-bold text-info">Quick Links</h5>
+                            <p><a href="#about" className="text-light text-decoration-none">About</a></p>
+                            <p><a href="#projects" className="text-light text-decoration-none">Projects</a></p>
+                            <p><a href="#skills" className="text-light text-decoration-none">Skills</a></p>
+                            <p><a href="#contact" className="text-light text-decoration-none">Contact</a></p>
+                        </div>
+
+
+                        <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                            <h5 className="text-uppercase mb-4 fw-bold text-info">Contact</h5>
+                            <p><i className="bi bi-envelope-fill me-2"></i> your@email.com</p>
+                            <p><i className="bi bi-telephone-fill me-2"></i> +123 456 7890</p>
+                            <p><i className="bi bi-geo-alt-fill me-2"></i> Your City, Country</p>
+                        </div>
+
+
+                        <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mt-3">
+                            <h5 className="text-uppercase mb-4 fw-bold text-info">Follow Me</h5>
+                            <a href="#" className="btn btn-outline-light btn-floating m-1" role="button"><i className="bi bi-github"></i></a>
+                            <a href="#" className="btn btn-outline-light btn-floating m-1" role="button"><i className="bi bi-linkedin"></i></a>
+                            <a href="#" className="btn btn-outline-light btn-floating m-1" role="button"><i className="bi bi-twitter"></i></a>
+                            <a href="#" className="btn btn-outline-light btn-floating m-1" role="button"><i className="bi bi-instagram"></i></a>
+                        </div>
+                    </div>
+
+                    <hr className="my-4 text-light"/>
+
+
+                        <div className="row">
+                            <div className="col-md-12 text-center">
+                                <p className="mb-0">© 2025 Your Name. All rights reserved.</p>
+                            </div>
+                        </div>
+                </div>
+            </footer>
+
         </>
     )
 }
